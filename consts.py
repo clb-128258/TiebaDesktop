7585f6a7947526b231a55812cbb021ca572387f0
clb-128258 <lao17341@gmail.com>
2025-11-17 09:43:59 +0800

    更新常量信息

--- consts.py
+++ consts.py
@@ -2,8 +2,8 @@
 import os
 
 enable_log_file = True
-APP_VERSION_STR = '1.1.2-beta'
-APP_VERSION_NUM = 4
+APP_VERSION_STR = '1.2.0-release'
+APP_VERSION_NUM = 5
 encrypt_key = 'G6WxHyBcliRT5KqcaLkskO5SKB3JJ9dX'
 datapath = './tiebadesktop_userdata'
 if os.name == 'nt':
